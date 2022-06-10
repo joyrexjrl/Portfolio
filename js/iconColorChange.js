300314bd29007c074b052a62d9b200e978ae79a7
joyrexjrl <ricklozano@gmail.com>
2022-06-10 16:46:15 -0700

    graphic added to background
    added the page icon graphic to the backgrounds of all the sections on the page. created js to change the color of the menu icon when section is in view. organized js files to own folder. fixed some resizing issues. created new name animation sections for both small and large screens

---
+++ js/iconColorChange.js
@@ -0,0 +1,44 @@
+const sections = document.querySelectorAll("section");
+const sectionOne = document.querySelector(".headSection");
+
+const options= {
+    rootMargin: "-25% 0% -25% 0%",
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {        
+        if(!entry.isIntersecting){
+            return;
+        }
+
+        if(entry.target === document.getElementById("intro")){
+            document.getElementById("imageIntro").classList.add("navLocation");
+        }else{
+            document.getElementById("imageIntro").classList.remove("navLocation");
+        }
+
+        if(entry.target === document.getElementById("about")){
+            document.getElementById("imageAbout").classList.add("navLocation");
+        }else{
+            document.getElementById("imageAbout").classList.remove("navLocation");
+        }
+
+        if(entry.target === document.getElementById("projects")){
+            document.getElementById("imageProjects").classList.add("navLocation");
+        }else{
+            document.getElementById("imageProjects").classList.remove("navLocation");
+        }
+
+        if(entry.target === document.getElementById("skills")){
+            document.getElementById("imageSkills").classList.add("navLocation");
+        }else{
+            document.getElementById("imageSkills").classList.remove("navLocation");
+        }                
+        //console.log(entry.target);
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
